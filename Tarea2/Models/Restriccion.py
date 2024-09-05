154261545da107000a114869d1eacb295dd9ff17
Rodrigo <rodri.mo@ciencias.unam.mx>
2024-09-05 11:01:28 -0600

    Implementando CSP 50%

---
+++ Tarea2/Models/Restriccion.py
@@ -0,0 +1,7 @@
+class Restriccion():
+    def __init__(self, v1, v2, valorRestringido) -> None:
+        self.v1 = v1
+        self.v2 = v2
+        self.valorRestringido = valorRestringido # Seguro que aqui puedo optimizar memoria lol
+    
+        
